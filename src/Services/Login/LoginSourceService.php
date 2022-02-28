60ae28f1c3b0fad397ce96f99c657c3c051af3c6
zevitagem <jose.victor@picpay.com>
2022-02-27 21:38:16 -0300

    Exibindo slugs no sourcer sem parametro

--- src/Services/Login/LoginSourceService.php
+++ src/Services/Login/LoginSourceService.php
@@ -6,9 +6,12 @@ use Zevitagem\LegoAuth\Services\Login\AbstractLoginService;
 
 class LoginSourceService extends AbstractLoginService
 {
-
     public function getDataOnShowLogin(array $config)
     {
+        if (empty($config)) {
+            $config['with_slugs'] = '1';
+        }
+
         return [
             'slugs' => $this->getSlugs($config)
         ];
